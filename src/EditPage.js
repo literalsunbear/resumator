bc20d7a251b00266f85e33340360f00ab3ab14d9
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-10-04 23:06:09 -0400

    Setup environment; initial sketch

---
+++ src/EditPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EditPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className='edit-page'>
+                <h1>this is the edit page</h1>
+            </div>
+        )
+    }
+}
+
+export default EditPage;
---
+++ src/PreviewPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PreviewPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className='preview-page'>
+            <h1>this is the preview page</h1>
+        </div>
+        )
+    }
+}
+
+export default PreviewPage;
