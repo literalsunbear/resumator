86baa80336d42aba094f257f4769c8a8790836c3
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-10-05 20:15:16 -0400

    updating state on input change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import EditPage from './EditPage';
 import PreviewPage from './PreviewPage';
 import './App.css';
 
@@ -32,8 +31,10 @@ class App extends React.Component {
                 edDescription: []
             }
         }
+
         this.handleEditPageToggle = this.handleEditPageToggle.bind(this);
         this.handlePreviewPageToggle = this.handlePreviewPageToggle.bind(this);
+        this.handleEditNameChange = this.handleEditNameChange.bind(this);
     }
 
     handleEditPageToggle() {
@@ -41,6 +42,8 @@ class App extends React.Component {
         const preview = document.querySelector('.preview-page');
         edit.classList.toggle('hide');
         preview.classList.toggle('show');
+
+        alert(`hey there, ${this.state.name}!`)
     }
     handlePreviewPageToggle() {
         const edit = document.querySelector('.edit-page');
@@ -48,12 +51,42 @@ class App extends React.Component {
         edit.classList.toggle('hide');
         preview.classList.toggle('show');
     }
+    handleEditNameChange(e) {
+        this.setState({ name: e.target.value });
+    }
+    handleEditTitleChange(e) {
+        this.setState({ title: e.target.value });
+    }
 
     render() {
         return(
             <>
-            <EditPage
-            toggle={this.handleEditPageToggle}/>
+            <div className='edit-page'>
+                <div
+                className='edit-page-toggle-btn'
+                onClick={()=>{this.handleEditPageToggle()}}>
+                    preview
+                </div>
+                {/* name, title and headshot */}
+                <div className='edit-hero-form'>
+                    <label for='name-input'>Your Name: </label>
+                    <input
+                    type='text'
+                    name='name-input'
+                    onChange={this.handleEditNameChange}></input>
+                    <label for='title-input'>Your Title: </label>
+                    <input
+                    type='text'
+                    name='title-input'
+                    onChange={this.handleEditTitleChange}></input>
+                    <label for='avi-input'>Choose a Headshot: </label>
+                    <input
+                    type='file'
+                    name='avi-input'></input>
+                </div>
+
+
+            </div>
             <PreviewPage
             toggle={this.handlePreviewPageToggle}/>
             </>
