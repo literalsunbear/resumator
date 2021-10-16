c7143d1c76b77da05ed4ab50b2bd14c97c07aa6e
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-10-16 14:33:04 -0400

    Playing with modals

---
+++ src/EditSkillModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EditSkillModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div
+            className='edit-skill-modal'>
+                <h1>this is the edit skill modal</h1>
+            </div>
+        )
+    }
+}
+export default EditSkillModal;
